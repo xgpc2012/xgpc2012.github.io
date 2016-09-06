9389c791972fb2a1c4aecc84ad699bee70479390
chang.peng@zymobi.com <chang.peng@zymobi.com>
2016-09-06 12:03:55 +0800

    Hello world

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by appcan on 2016/9/6.
+ */
+require.config({
+    baseUrl:"js/lib",
+    paths: {
+        swiper: 'swiper-3.3.1.min'
+    }
+});
+
+require(["swiper"], function() {
+    //初始化首页轮播图
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        // 分页器
+        pagination: '.swiper-pagination',
+        // 前进后退按钮
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+
+    })
+});
