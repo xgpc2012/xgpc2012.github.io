f9e9bed4e8b7a78e460b7fd779f050133d82bc44
chang.peng@zymobi.com <chang.peng@zymobi.com>
2016-09-07 15:30:12 +0800

    Hello world

--- js/main.js
+++ js/main.js
@@ -2,27 +2,34 @@
  * Created by appcan on 2016/9/6.
  */
 require.config({
-    baseUrl:"js/lib",
+    baseUrl: "js/lib",
     paths: {
-        swiper: 'swiper-3.3.1.min'
+        swiper: "swiper-3.3.1.min",
+        jquery:"http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min",
     }
 });
 
-require(["swiper"], function() {
+require(["swiper"], function () {
     //初始化首页轮播图
-    var mySwiper = new Swiper ('.swiper-container', {
+    var mySwiper = new Swiper('.swiper-container', {
         autoplay: 2000,
-        effect : 'fade',
-        autoplayDisableOnInteraction : false,
+        effect: 'fade',
+        autoplayDisableOnInteraction: false,
+        autoplayStopOnLast: true,
         direction: 'horizontal',
-        loop: true,
         // 分页器
         pagination: '.swiper-pagination',
         // 前进后退按钮
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
-        onClick: function(swiper){
+        onClick: function (swiper) {
             mySwiper.slideNext();
         }
     })
 });
+
+require(["jquery"], function () {
+    $("#go").on("click", function () {
+        window.location.href="home.html";
+    })
+})
