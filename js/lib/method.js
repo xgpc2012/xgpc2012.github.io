8b234ccafaf3e3a2ebd52de7e926da4e49048537
chang.peng@zymobi.com <chang.peng@zymobi.com>
2016-11-02 17:36:00 +0800

    Hello world

--- js/lib/method.js
+++ js/lib/method.js
@@ -2,10 +2,104 @@
  * Created by appcan on 2016/9/8.
  */
 define(function () {
-    var add= function (a,b) {
-        return a+b;
-    }
+    var timer = null;
+    var alpha = 20;
+    //跨浏览器的事件处理程序
+    var EventUtil = {
+        //添加事件
+        addHandler: function (element, type, handler) {
+            if (element.addEventListener) {
+                element.addEventListener(type, handler, false);
+            } else if (element.attachEvent) {
+                element.attachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = handler;
+            }
+        },
+        getEvent: function (event) {
+            return event ? event : window.event;
+        },
+        getTarget: function (event) {
+            return event.target || event.srcElement;
+        },
+        preventDefault: function (event) {
+            if (event.preventDefault) {
+                event.preventDefault();
+            } else {
+                event.returnValue = false;
+            }
+        },
+        //移除事件
+        removeHandler: function (element, type, handler) {
+            if (element.removeEventListener) {
+                element.removeEventListener(type, handler, false);
+            } else if (element.detachEvent) {
+                element.detachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = null;
+            }
+        },
+        stopPropagation: function (event) {
+            if (event.stopPropagation) {
+                event.stopPropagation();
+            } else {
+                event.cancelBubble = true;
+            }
+        }
+    };
+
+    //侧边栏收缩展开
+    var startMove = function (iTarget, speed, el) {
+        clearInterval(timer);
+        timer = setInterval(function () {
+            if (el.offsetLeft == iTarget) {
+                clearInterval(timer);
+            } else {
+                el.style.left = el.offsetLeft + speed + "px";
+            }
+        }, 30)
+    };
+
+    //淡入淡出
+    var startChange = function (iTarget, speed, el) {
+        clearInterval(timer);
+        timer = setInterval(function () {
+            if (alpha == iTarget) {
+                clearInterval(timer);
+            } else {
+                alpha += speed;
+                el.style.filter = "alpha(opacity:" + alpha + ")";
+                el.style.opacity = alpha / 100;
+            }
+        }, 50)
+    };
+    var AJAX = function (url, type, data) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            console.log(xhr.readyState + " " + new Date());
+        }
+        xhr.onload = function (event) {
+            if (xhr.readyState == 4) {
+                if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+                    console.log(xhr.responseText);
+                } else {
+                    console.log("Request was unsuccessful: " + xhr.status);
+                }
+            }
+        };
+        xhr.onprogress = function (event) {
+            var divStatus = document.getElementById("status");
+            if (event.loaded) {
+
+            }
+        };
+        xhr.open(type, url, true);
+        xhr.send(data);
+    };
     return {
-        add:add
+        startMove: startMove,
+        startChange: startChange,
+        AJAX:AJAX,
+        EventUtil:EventUtil
     }
 })
--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ require.config({
     baseUrl: "js/lib",
     paths: {
         swiper: "swiper-3.3.1.min",
-        jquery:"https://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min",
+        jquery: "https://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min",
     }
 });
 
@@ -30,10 +30,31 @@ require(["swiper"], function () {
 
 require(["jquery"], function () {
     $("#go").on("click", function () {
-        window.location.href="home.html";
+        window.location.href = "home.html";
     })
 })
 
 require(["method"], function (m) {
-    console.log(m.add(1,3));
+    var ev = m.EventUtil;
+    //侧边栏运动
+    var div1 = document.getElementById("div1");
+    ev.addHandler(div1, "mouseover", function () {
+        m.startMove(0, 10, div1);
+    })
+    ev.addHandler(div1, "mouseout", function () {
+        m.startMove(-150, -10, div1);
+    })
+
+    var request1 = document.getElementById("request1");
+    request1.addEventListener("click", function () {
+        m.AJAX("https://route.showapi.com/9-2?showapi_appid=5668&showapi_sign=000bca6e40344eafb11c68cf28a8bf43&area=武汉", "get", null);
+    }, false);
+
+    var oDiv2 = document.getElementById("div2");
+    oDiv2.onmouseover = function () {
+        m.startChange(100, 10, oDiv2);
+    }
+    oDiv2.onmouseout = function () {
+        m.startChange(20, -10, oDiv2);
+    }
 })
