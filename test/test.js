25113a85a1ef3ff4645cbb17c566fdc0806f55ab
pc <605123543@qq.com>
2017-07-13 10:22:53 +0800

    QA首页更新

--- test/test.js
+++ test/test.js
@@ -3,7 +3,6 @@
  */
 function moveToApp(){
     var uri="umsapp://main.app/openwith?a=1&b=2";
-    alert("跳转地址是:"+uri);
     //此处应当先判断设备类型
     //location.href="http://www.baidu.com";
     location.href=uri;
