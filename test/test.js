89cdcaefcdd08e6f7028923061aa13b35f8b0ee5
pc <605123543@qq.com>
2017-07-13 10:46:32 +0800

    QA首页更新

--- test/test.js
+++ test/test.js
@@ -1,13 +1,17 @@
 /**
  * Created by pc on 2017/7/13.
  */
-function moveToApp(mobile){
-    var uri="umsapp://main.app/openwith?";
-    var params="a=1&b=2";
-    var u = navigator.userAgent;
-    var isAndroid=u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
-    alert(isAndroid);
+function moveToApp(mobile) {
+    var u = navigator.userAgent,
+        uri = "",
+        params = "key1=1&key2=2";
+    //终端类型判断
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+    if (isAndroid) {
+        uri = "umsapp://main.app/openwith?";
+    } else {
+        uri = "umsylsw://kouliang?";
+    }
     //此处应当先判断设备类型
-    //location.href="http://www.baidu.com";
-    //location.href=uri;
+    location.href = uri + encodeURI(params);
 }
