87330f1eb002343297cc0a3b1e8a643c3051d414
pc <605123543@qq.com>
2017-07-13 10:19:48 +0800

    QA首页更新

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by pc on 2017/7/13.
+ */
+function moveToApp(){
+    var uri="umsapp://main.app/openwith?a=1&b=2";
+
+    //此处应当先判断设备类型
+    //location.href="http://www.baidu.com";
+    location.href=uri;
+}
