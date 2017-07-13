205be965ce7b94534ed0bc36b498da0f12233ee6
pc <605123543@qq.com>
2017-07-13 10:36:08 +0800

    QA首页更新

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 /**
  * Created by pc on 2017/7/13.
  */
-function moveToApp(){
-    var uri="umsapp://main.app/openwith?a=1&b=2";
+function moveToApp(mobile){
+    var uri="umsapp://main.app/openwith?";
+    var params="a=1&b=2";
+    var userAgent = navigator.userAgent;
+    alert(userAgent);
     //此处应当先判断设备类型
     //location.href="http://www.baidu.com";
-    location.href=uri;
+    //location.href=uri;
 }
