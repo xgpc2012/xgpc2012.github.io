6b16dddae1478a622cb32468dda6ac94db8f00d0
pc <605123543@qq.com>
2017-07-17 11:06:24 +0800

    QA首页更新

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /**
  * Created by pc on 2017/7/13.
  */
-function moveToApp(e,mobile) {
+function moveToApp(mobile) {
     var u = navigator.userAgent,
         uri = "",
         params = "key1=1&key2=2";
