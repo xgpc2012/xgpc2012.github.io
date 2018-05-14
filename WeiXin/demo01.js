b625cda49f040ba35b0d57d5ef52b1f1fb853122
pc <605123543@qq.com>
2018-05-14 16:55:49 +0800

    更新画进度条

--- WeiXin/demo01.js
+++ WeiXin/demo01.js
@@ -1,24 +1,26 @@
 /**
  * Created by pc on 2018/2/8.
  */
-wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: "wxf9bcb16718854ae1", // 必填，公众号的唯一标识
-    timestamp: '', // 必填，生成签名的时间戳
-    nonceStr: '', // 必填，生成签名的随机串
-    signature: '',// 必填，签名
-    jsApiList: ["scanQRCode"] // 必填，需要使用的JS接口列表
-});
+var appId = "wxf9bcb16718854ae1";
+function registerWX(json) {
+    wx.config({
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: appId, // 必填，公众号的唯一标识
+        timestamp: json["timestamp"], // 必填，生成签名的时间戳
+        nonceStr: json["nonceStr"], // 必填，生成签名的随机串
+        signature: json["signature"],// 必填，签名
+        jsApiList: ["scanQRCode"] // 必填，需要使用的JS接口列表
+    });
+    wx.ready(function () {
+        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+        console.log("微信注册配置成功");
+    });
 
-wx.ready(function () {
-    // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-    console.log("配置成功");
-});
-
-wx.error(function (res) {
-    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-    console.log("配置失败");
-});
+    wx.error(function (res) {
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+        console.log("微信注册配置失败");
+    });
+}
 
 function scan() {
     wx.scanQRCode({
@@ -30,16 +32,32 @@ function scan() {
     });
 }
 
-function getAccessToken() {
+function getAccessToken(callback) {
     $.ajax({
-        url: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf9bcb16718854ae1&secret=3a3ef361574bf7a26014eedfc473e5c0',
+        url: 'http://144.131.252.121:8080/wechat/getTicket.do',
         type: "GET",
         dataType: "jsonp",
+        jsonp: "jsoncallback",
         success: function (data) {
-            alert(JSON.stringify(data));
+            callback && callback(data);
         },
         error: function (err) {
-            alert(err);
+            console.log("获取token失败:" + JSON.stringify(err));
+        }
+    })
+}
+
+function getSign(callback) {
+    $.ajax({
+        url: 'http://144.131.252.121:8080/wechat/sign.do',
+        type: "GET",
+        dataType: "jsonp",
+        jsonp: "jsoncallback",
+        success: function (data) {
+            callback && callback(data);
+        },
+        error: function (err) {
+            console.log("获取签名失败:" + JSON.stringify(err));
         }
     })
 }
